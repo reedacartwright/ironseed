9bd16339168fb679d3badc854f8f515bf676c25c
Reed A. Cartwright <cartwright@asu.edu>
2025-07-21 00:18:51 -0700

    Configure tinytest to use at_home tests.

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,3 +1,4 @@
 if (requireNamespace("tinytest", quietly = TRUE)) {
-  tinytest::test_package("ironseed")
+  home <- length(unclass(packageVersion("ironseed"))[[1]]) == 4
+  tinytest::test_package("ironseed", at_home = home)
 }
