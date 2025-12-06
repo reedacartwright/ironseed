8ba1893dbb1a1d03f5d0d400974ececfa905a775
Reed A. Cartwright <cartwright@asu.edu>
2025-12-06 12:32:23 -0700

    Add tests for digest()

---
+++ inst/tinytest/test_digest.R
@@ -0,0 +1,34 @@
+zero <- as.raw(c(0, 0, 0, 0))
+one <- as.raw(c(1, 0, 0, 0))
+hund <- as.raw(c(100, 0, 0, 0))
+hundbe <- as.raw(c(0, 0, 0, 100))
+
+msg_one01 <- -1238509685
+msg_zero01 <- -398917142
+msg_hund100_100 <- -1221982725
+msg_hundbe100_100 <- -1125694476
+
+msg_zero10 <- c(
+  -2027064858L, -1489326973L, 1142046877L, -406317157L, -1521925087L,
+  -213636100L, -1491661455L, 637854673L, -1153662057L, -576691921L
+)
+
+expect_equal(digest(zero, 0, serialize = FALSE), integer(0L))
+
+expect_equal(digest(zero, 1, serialize = FALSE), msg_zero01)
+expect_equal(digest(one, 1, serialize = FALSE), msg_one01)
+expect_equal(digest(zero, 10, serialize = FALSE), msg_zero10)
+
+expect_equal(digest(hund, 100, serialize = FALSE)[100], msg_hund100_100)
+expect_equal(digest(hundbe, 100, serialize = FALSE)[100], msg_hundbe100_100)
+
+# hund_obj: serialize(100L, connection = NULL, ascii = FALSE, xdr = FALSE)
+hund_obj <- as.raw(c(
+  0x42, 0x0a, 0x03, 0x00, 0x00, 0x00, 0x02, 0x05, 0x04,
+  0x00, 0x00, 0x05, 0x03, 0x00, 0x05, 0x00, 0x00, 0x00, 0x55, 0x54,
+  0x46, 0x2d, 0x38, 0x0d, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+  0x64, 0x00, 0x00, 0x00
+))
+
+expect_equal(digest(hund_obj, serialize = FALSE), -122472043)
+expect_equal(digest(100L), -122472043)
